95dfa1edbbbffc87734b7ad0cb7d65ddcaf1f722
Jack Zhang <dothide@gmail.com>
2016-04-13 21:38:54 +0800

    chore(Grunt): Config Gruntfile & added wiredep

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,33 @@
 'use strict';
 
 module.exports = function(grunt) {
+
+  // show elapsed time at the end
+  require('time-grunt')(grunt);
+  // load all grunt tasks
+  require('load-grunt-tasks')(grunt);
   
   //Project configuration
   grunt.initConfig({
-    
+    // Define Datas
+    app: {
+      name: 'ankiplus',
+      src_path: 'app/',
+      dist_path: 'dist/',
+      test_path: 'spec/' 
+    },
+
+    // Config Tasks
+    wiredep: {
+      // Dev Target
+      dev: {
+        src: ['index.html']
+      }
+    }
   });
+
+  // Register Tasks
+  grunt.registerTask('default', [
+    'wiredep:dev'
+  ]);
 }
